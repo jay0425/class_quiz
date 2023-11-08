e690c232e3b0583ed59f07077af29f74ab1aa549
jay0425 <heedev94@gmail.com>
2023-11-08 21:15:51 +0900

    API 통신 연습

---
+++ pages/04-2/index.js
@@ -0,0 +1,52 @@
+import { gql, useMutation } from '@apollo/client';
+import { useState } from 'react';
+
+const CREATE_BOARD = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function ExamplePage0402() {
+  const [writer, setWriter] = useState('');
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await createBoard({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      작성자 : <input type="text" onChange={onChangeWriter} />
+      제목 : <input type="text" onChange={onChangeTitle} />
+      내용 : <textarea onChange={onChangeContents} />
+      <button onClick={onClickSubmit}>[ GRAPHQL-API 요청하기 ]</button>
+    </>
+  );
+}
